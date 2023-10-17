b05e1f6289634f1cc8d0e8064b08dc5066ff83f7
karen12138 <360215505@qq.com>
2023-10-17 18:04:21 +0800

    Flask学习

--- day3.py
+++ day3.py
@@ -1,27 +1,26 @@
-# Day2
-## Jinja2模板
-1. <font size = 3>渲染模板 
-> render_template()
-2. 模板传参 
-> {{  }}
-3. 过滤器(对变量进行函数操作) 
-> {{name|length}}、app.templates.filter("过滤器名")
-4. 控制语句
->{% %} 结束语句：{% endif %}、{% endfor %}
-5. 模板继承 
-* 父模板{% block head %} {% endblock%}  
-* 子模版{% extends "base.html" %} 、{% block head %} ... {% endblock%}
-6. 加载静态文件 
-> static
-</font>
+# 1、2023/10/15 今天要学习的是flask-SQLAlchemy连接数据库
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
-# Day3 
-## 3.1Flask-SQLAlchemy [连接数据库](https://www.bilibili.com/video/BV17r4y1y7jJ?p=14&vd_source=33207922e975d5ad1770261da92cead1)
-## 3.2 ORM模型
-* <font size = 3>__一个ORM模型对应一个数据表__
-* ORM模型的属性对应表内的每个字段</font>
-<font size = 3>
-~~~
+app = Flask(__name__)
+
+HOSTNAME = "127.0.0.1",
+PORT = 3306
+USERNAME = "root",
+PASSWORD = "lkl",
+DATABASE = "flask_learn"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:lkl@127.0.0.1:3306/flask_learn?charset=utf8"
+
+db = SQLAlchemy(app)
+
+# with app.app_context():
+#     with db.engine.connect() as conn:
+#         rs = conn.execute(text("select 1"))
+#         print(rs.fetchone())
+
+# 2、创建数据表
 class User(db.Model):
     ## 数据库的表名
     __tablename__ = "user"
@@ -33,28 +32,26 @@ class User(db.Model):
 
 with app.app_context():
     db.create_all()
-~~~  
- </font>
 
-## 3.3 使用ORM模型增查改删  
+@app.route('/')
+def hello_world():  # put application's code here
+    return "Hello World!"
 
-* <font size = 3>创建数据
-~~~
-@app.route('/user/add')
-def user_add():
-    ## 创建实例对象
-    user1 = User(username = "lkl2", password = 111111)
-    user2 = User(username="gqx", password=222222)
-    ## 添加到会话db.session中
-    db.session.add(user1)
-    db.session.add(user2)
-    ## 同步到数据库中
-    db.session.commit()
-    return "用户添加成功"
-~~~
- 
-* 查询数据
-~~~
+# # 3、增查改删的数据操作
+# ## 3.1 创建数据
+# @app.route('/user/add')
+# def user_add():
+#     ## 创建实例对象
+#     user1 = User(username = "lkl2", password = 111111)
+#     user2 = User(username="gqx", password=222222)
+#     ## 添加到会话db.session中
+#     db.session.add(user1)
+#     db.session.add(user2)
+#     ## 同步到数据库中
+#     db.session.commit()
+#     return "用户添加成功"
+
+## 3.2 查询数据
 @app.route('/user/query')
 def user_query():
     ### 提取方法：获取主键查询（主键是能确定一条记录的唯一标识）
@@ -65,9 +62,8 @@ def user_query():
     for user in users:
         print( f"{user.username}:{user.password}")
     return "用户查询成功"
-~~~
-* 更新数据
-```
+
+## 3.3 更新数据
 @app.route('/user/update')
 def user_update():
     ### first()获取到的是当前username下的整个数据
@@ -78,14 +74,17 @@ def user_update():
     user = User.query.get(3)
     print((user.password))
     return "数据更新成功"
-```
-* 删除数据
- ```
- @app.route("/user/delete")
+
+## 3.4 删除数据
+@app.route("/user/delete")
 def user_delete():
     user = User.query.filter_by(username="lkl2").first()
     db.session.delete(user)
     ## 推送到数据库同步
     db.session.commit()
     return "数据删除成功"
- ```
+
+# 2023/10/17 今天学的是外键与表关系
+
+if __name__ == '__main__':
+    app.run(debug=True)
