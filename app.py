e2e759f7c0848972dd65a2632f4fe3af2c31c816
karen12138 <360215505@qq.com>
2023-10-18 15:56:09 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+## 视图函数
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template("child.html")
