3ef83a2d774c068c577a13a8b4f143e381d16a4b
karen12138 <360215505@qq.com>
2023-11-02 17:12:48 +0800

    更新 day5_app.py

--- Day5/day5_app.py
+++ Day5/day5_app.py
@@ -1,10 +1,11 @@
 # 问答平台项目搭建
-
 from flask import Flask
-from Day5 import config
+import config
 from exts import db
+from models import UserModel
 from blueprints.q_a import bp as qa_bp
 from blueprints.user import bp as user_bp
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 ## 1. 绑定配置文件
@@ -12,6 +13,9 @@ app.config.from_object(config)
 
 ## 2. 先创建db，到后面再绑定到app
 db.init_app(app)
+
+## 初始化Flask-Migrate，将程序和数据库关联在一起，便于实时更新数据表
+migrate = Migrate(app, db)
 
 ## 3. 将蓝图引用到app里面
 app.register_blueprint(qa_bp)
@@ -21,4 +25,4 @@ app.register_blueprint(user_bp)
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
