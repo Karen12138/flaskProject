33bb6fb2f38a4fd3fa4365480f77dd4ea6c7010d
karen12138 <360215505@qq.com>
2023-11-02 17:12:46 +0800

    更新 config.py

---
+++ Day5/config.py
@@ -0,0 +1,9 @@
+## 数据库配置信息
+HOSTNAME = "127.0.0.1",
+PORT = 3306
+USERNAME = "root",
+PASSWORD = "lkl",
+DATABASE = "flask_oa"
+
+DB_URI = "mysql+pymysql://root:lkl@127.0.0.1:3306/flask_oa?charset=utf8"
+SQLALCHEMY_DATABASE_URI = DB_URI
